ae25c4162a1501f3692cbdbd1f4f26859dd4bf3d
palak029 <96968014+palak029@users.noreply.github.com>
2022-07-11 18:09:15 +0530

    Update reviewModel.js

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -1,5 +1,5 @@
 const mongoose=require('mongoose');
-const db_link='mongodb+srv://Admin:TI4FAedLDtZ1M2RD@cluster0.fo1c8nb.mongodb.net/?retryWrites=true&w=majority"';
+const db_link='mongodb+srv://Admin:TI4FAedLDtZ1M2RD@cluster0.fo1c8nb.mongodb.net/?retryWrites=true&w=majority';
 mongoose.connect(db_link)
 .then(function(db){
   // console.log(db);
